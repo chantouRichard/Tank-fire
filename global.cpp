5f384f2c18c981cf15ee545cf52197c168e0ead5
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2024-07-03 09:18:40 +0800

    全局变量的头文件和cpp文件
    对地图变量，设置按键等进行定义和赋值

---
+++ global.cpp
@@ -0,0 +1,9 @@
+#include "global.h"
+#include<Qt>
+
+int gameVolume=50;
+int KeyUp=Qt::Key_W;
+int KeyLeft=Qt::Key_A;
+int KeyRight=Qt::Key_D;
+int KeyDown=Qt::Key_S;
+int KeyShoot=Qt::Key_Space;
---
+++ global.h
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern int gameVolume;
+extern int KeyUp;
+extern int KeyLeft;
+extern int KeyRight;
+extern int KeyDown;
+extern int KeyShoot;
+
+#endif // GLOBAL_H
