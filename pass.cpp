80c0b1f8a0b199a6dc0df1c6bac00352c1b2bedd
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-03 19:40:12 +0800

    Create pass.cpp

---
+++ pass.cpp
@@ -0,0 +1,347 @@
+#include "pass.h"
+#include "ui_pass.h"
+#include"level1.h"
+#include"level2.h"
+#include"level3.h"
+#include<QPushButton>
+#include<QTimer>
+#include<QVBoxLayout>
+#include<QIcon>
+#include<QLabel>
+#include"level4.h"
+
+
+
+pass::pass(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::pass)
+{
+    ui->setupUi(this);
+    setFixedSize(1200,800);
+    setWindowTitle("tankfire");
+//    this->setStyleSheet("background-color: blue;");
+    QLabel *backgroundLabel = new QLabel(this);
+
+    backgroundLabel->setScaledContents(true); // 这将使图片缩放以填充QLabel的整个区域
+    backgroundLabel->resize(this->size()); // 确保QLabel的大小与父窗口或控件相同
+    backgroundLabel->setPixmap(QPixmap(":/1/Res/res/gameover.jpg"));
+
+    level1* Level1=new level1;
+    level2* Level2=new level2;
+    level3* Level3=new level3;
+    level4* Level4=new level4;
+
+    //设置按钮
+//    QIcon icon("C:/Users/qq864/Downloads/backbtn.png");
+    backbtn=new QPushButton("Back");
+    backbtn->setParent(this);
+    backbtn->move(10,10);
+    backbtn->resize(100,50);
+
+//
+
+
+    //level1
+    QPushButton * l1btn=new QPushButton("level1",this);
+    l1btn->resize(160,80);
+    l1btn->move(this->width()/5-l1btn->width()/2,this->height()/2-l1btn->height()/2);
+    l1btn->setStyleSheet(
+        "QPushButton {"
+        "   background-color: red;"
+        "   color: white;"
+        "   border: none;"
+        "   padding: 10px 20px;"
+        "}"
+        "QPushButton:hover {"
+        "   background-color: pink;"
+        "}"
+        "QPushButton:pressed {"
+        "   background-color: red;"
+        "}"
+    );
+    //level2
+    QPushButton * l2btn=new QPushButton("level2",this);
+    l2btn->resize(160,80);
+    l2btn->move(this->width()*2/5-l2btn->width()/2,this->height()/2-l2btn->height()/2);
+    setColor(l2btn);
+    //level3
+    QPushButton * l3btn=new QPushButton("level3",this);
+    l3btn->resize(160,80);
+    l3btn->move(this->width()*3/5-l3btn->width()/2,this->height()/2-l3btn->height()/2);
+    setColor(l3btn);
+    //level4
+    QPushButton * l4btn=new QPushButton("level4",this);
+    l4btn->resize(160,80);
+    l4btn->move(this->width()*4/5-l4btn->width()/2,this->height()/2-l4btn->height()/2);
+    setColor(l4btn);
+    //未解锁显示
+
+    QFont font = backbtn->font();
+    QFont font1 = l1btn->font();
+    QFont font2 = l2btn->font();
+    QFont font3 = l3btn->font();
+    QFont font4 = l4btn->font();
+    //字体设置
+    font.setPointSize(20);
+    font1.setPointSize(20);
+    font2.setPointSize(20);
+    font3.setPointSize(20);
+    font4.setPointSize(20);
+
+    //设置成未解锁状态
+    l2btn->setEnabled(false);
+    l3btn->setEnabled(false);
+    l4btn->setEnabled(false);
+
+    //初始化connect
+    setupLevel0Button(l1btn,Level1,l2btn);
+    setupLevel1Button(l2btn,Level2,l3btn);
+    setupLevel2Button(l3btn,Level3,l4btn);
+    setupLevel3Button(l4btn,Level4);
+
+//    connect(l1btn,&QPushButton::clicked,[=](){
+//        QTimer::singleShot(500,this,[=](){
+//            this->hide();
+//            Level1->show();
+
+//            connect(Level1,&level1::Win,[=](){
+//                pass_judge[0]=true;
+//                setupLevel1Button(l2btn,Level2,l3btn);
+//                colorChange(l2btn);
+//                this->show();
+//                Level1->close();
+//                l2btn->clicked();
+//                this->hide();
+//            });
+
+//            connect(Level1,&level1::back,this,[=](){
+//                this->show();
+//                Level1->close();
+//            });
+//        });
+
+//    });
+    //第二关按钮触发
+    if(pass_judge[0])
+    {
+        l2btn->setEnabled(true);
+//        connect(l2btn,&QPushButton::clicked,[=](){
+//            QTimer::singleShot(500,this,[=](){
+//                this->hide();
+//                Level2->show();
+
+//                connect(Level2,&level2::Win,[=](){
+//                    pass_judge[1]=true;
+//                    setupLevel2Button(l3btn,Level3,l4btn);
+//                    colorChange(l2btn);
+//                    this->show();
+//                    Level2->close();
+//                    l3btn->clicked();
+//                    this->hide();
+//                });
+
+//                connect(Level2,&level2::back,this,[=](){
+//                    this->show();
+//                    Level2->close();
+//                });
+//            });
+
+//        });
+    }
+    //第三关按钮触发
+    if(pass_judge[1])
+    {
+        l3btn->setEnabled(true);
+//        connect(l3btn,&QPushButton::clicked,[=](){
+//            QTimer::singleShot(500,this,[=](){
+//                this->hide();
+//                Level2->show();
+
+//                connect(Level3,&level3::Win,[=](){
+//                    pass_judge[2]=true;
+//                    setupLevel3Button(l4btn,Level4);
+//                    colorChange(l3btn);
+//                    this->show();
+//                    Level3->close();
+//                    l4btn->clicked();
+//                    this->hide();
+//                });
+
+//                connect(Level3,&level3::back,this,[=](){
+//                    this->show();
+//                    Level3->close();
+//                });
+//            });
+
+//        });
+    }
+    //第四关按钮触发
+    if(pass_judge[2])
+    {
+        l4btn->setEnabled(true);
+//        connect(l4btn,&QPushButton::clicked,[=](){
+//            QTimer::singleShot(500,this,[=](){
+//                this->hide();
+//                Level4->show();
+
+//                connect(Level4,&level4::Win,[=](){
+//                    pass_judge[3]=true;
+//                    colorChange(l4btn);
+//                    this->show();
+//                    Level4->close();
+
+//                });
+
+//                connect(Level4,&level4::back,this,[=](){
+//                    this->show();
+//                    Level4->close();
+//                });
+//            });
+
+//        });
+    }
+    connect(backbtn,&QPushButton::clicked,[=](){
+        QTimer::singleShot(500,this,[=](){
+            emit this->back();
+        });
+    });
+
+    //按钮的重新配色
+
+}
+void pass::setupLevel0Button(QPushButton *button, level1 *level, QPushButton *nextButton)
+{
+    connect(button,&QPushButton::clicked,[=](){
+        QTimer::singleShot(500,this,[=](){
+            this->hide();
+            level->show();
+
+            connect(level,&level1::Win,[=](){
+                pass_judge[0]=true;
+                colorChange(nextButton);
+                nextButton->setEnabled(true);
+                this->show();
+                level->close();
+                nextButton->clicked();
+                this->hide();
+            });
+
+            connect(level,&level1::back,this,[=](){
+                this->show();
+                level->close();
+            });
+        });
+
+    });
+}
+void pass::setupLevel1Button(QPushButton *button, level2 *level, QPushButton *nextButton)
+{
+    connect(button, &QPushButton::clicked, [=]() {
+        QTimer::singleShot(500, this, [=]() {
+            this->hide();
+            level->show();
+
+            connect(level, &level2::Win, [=]() {
+                pass_judge[1] = true;
+                colorChange(button);
+                nextButton->setEnabled(true);
+                this->show();
+                level->close();
+                nextButton->click();
+                this->hide();
+            });
+
+            connect(level, &level2::back, this, [=]() {
+                this->show();
+                level->hide();
+            });
+        });
+    });
+}
+void pass::setupLevel2Button(QPushButton *button, level3 *level, QPushButton *nextButton)
+{
+    connect(button, &QPushButton::clicked, [=]() {
+        QTimer::singleShot(500, this, [=]() {
+            this->hide();
+            level->show();
+
+            connect(level, &level3::Win, [=]() {
+                pass_judge[2] = true;
+                colorChange(button);
+                nextButton->setEnabled(true);
+                this->show();
+                level->close();
+                nextButton->click();
+                this->hide();
+            });
+
+            connect(level, &level3::back, this, [=]() {
+                this->show();
+                level->hide();
+            });
+        });
+    });
+}
+void pass::setupLevel3Button(QPushButton *button, level4 *level)
+{
+    connect(button,&QPushButton::clicked,[=](){
+        QTimer::singleShot(500,this,[=](){
+            this->hide();
+            level->show();
+
+            connect(level,&level4::Win,[=](){
+                pass_judge[3]=true;
+                colorChange(button);
+                this->show();
+                level->close();
+
+            });
+
+            connect(level,&level4::back,this,[=](){
+                this->show();
+                level->close();
+            });
+        });
+
+    });
+}
+void pass::setColor(QPushButton * btn)
+{
+    btn->setStyleSheet(
+            "QPushButton {"
+            "   background-color: lightgrey;"
+            "   color: white;"
+            "   border: none;"
+            "   padding: 10px 20px;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: grey;"
+            "}"
+            "QPushButton:pressed {"
+            "   background-color: lightgrey;"
+            "}"
+    );
+}
+void pass::colorChange(QPushButton * btn)
+{
+
+        btn->setStyleSheet(
+            "QPushButton {"
+            "   background-color: red;"
+            "   color: white;"
+            "   border: none;"
+            "   padding: 10px 20px;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: pink;"
+            "}"
+            "QPushButton:pressed {"
+            "   background-color: red;"
+            "}"
+        );
+}
+
+pass::~pass()
+{
+    delete ui;
+}
