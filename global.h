11df1f45042a2d2e649902482c985088667b68aa
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2024-07-03 14:27:29 +0800

    Update global.h

--- global.h
+++ global.h
@@ -1,5 +1,6 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
+#include<QString>
 
 extern int gameVolume;
 extern int KeyUp;
@@ -7,5 +8,11 @@ extern int KeyLeft;
 extern int KeyRight;
 extern int KeyDown;
 extern int KeyShoot;
+extern int Mapx_size;
+extern int Mapy_size;
+extern int map_Lattice_count;
+extern int My_speed;
+extern int enemy_speed;
+extern QString filename;
 
 #endif // GLOBAL_H
