bc8cbddd50e48a14cc83e1ce875b9f8cbb09227a
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2024-07-03 14:27:48 +0800

    Update global.cpp

--- global.cpp
+++ global.cpp
@@ -7,3 +7,7 @@ int KeyLeft=Qt::Key_A;
 int KeyRight=Qt::Key_D;
 int KeyDown=Qt::Key_S;
 int KeyShoot=Qt::Key_Space;
+int Mapx_size=1500;
+int Mapy_size=1500;
+int map_Lattice_count=25;
+QString filename=":/UserPass/User.txt";
