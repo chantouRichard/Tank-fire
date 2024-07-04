2d23bd4133a80167b7534a21aebcefad512a164f
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2024-07-04 13:02:28 +0800

    Update bullet.h

--- bullet.h
+++ bullet.h
@@ -15,7 +15,7 @@ public:
     explicit Bullet(QWidget *parent = nullptr);
     QTimer *bullettimer;
     void stoptimer();
-    int type;
+    int type;    //子弹移动方向
     int MAP[25][25];
     void Loadmap(int map[25][25]);
     QPixmap BULL;
@@ -23,17 +23,14 @@ public:
     void movebullet(QWidget*parent,int style,int x,int y);//发射子弹的移动
     bool checkmovebullet();//检查子弹是否可以继续移动
     void updatemapsit();
-    int bux;int buy;//记录子弹的位置
-    int mapbux;int mapbuy;
+    int bux;int buy;//记录子弹的像素位置
+    int mapbux;int mapbuy;    //子弹的格子位置
     int started; //记录是否开始计时
     void Disconnected();
     void moveup();
     void movedown();
     void moveleft();
     void moveright();
-
-signals:
-
 };
 
 #endif // BULLET_H
