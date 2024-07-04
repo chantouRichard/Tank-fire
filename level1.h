e2215bced5738143dd9dba5bbb53aed26f4f6370
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-04 09:04:33 +0800

    Update level1.h

--- level1.h
+++ level1.h
@@ -15,11 +15,19 @@ public:
     explicit level1(QWidget *parent = nullptr);
     ~level1();
     void paintEvent(QPaintEvent *event) override;
-
 signals:
+    void Win();
     void back();
 private:
     Ui::level1 *ui;
+
+    bool Pass=false;//判断是否通过该关卡
+    bool mainTankIsDead=false;
+    void initTank();
+    void initEnemyTank();
+    void dead();
+
+
 };
 
 #endif // LEVEL1_H
