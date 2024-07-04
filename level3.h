7bb2526964ebcd0016c76871a6cdebb1bc71d5dc
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-04 10:43:35 +0800

    Update level3.h

--- level3.h
+++ level3.h
@@ -1,33 +1,27 @@
 #ifndef LEVEL3_H
 #define LEVEL3_H
 
+#include "mainlevel.h"
 #include <QWidget>
 
 namespace Ui {
 class level3;
 }
 
-class level3 : public QWidget
-{
+class level3 : public mainlevel {
     Q_OBJECT
 
 public:
     explicit level3(QWidget *parent = nullptr);
     ~level3();
     void paintEvent(QPaintEvent *event) override;
-signals:
-    void Win();
-    void back();
+
 private:
     Ui::level3 *ui;
 
-    bool Pass=false;//判断是否通过该关卡
-    bool mainTankIsDead=false;
-    void initTank();
-    void initEnemyTank();
-    void dead();
-
-
+    void initTank() override;
+    void initEnemyTank() override;
+    void dead() override;
 };
 
 #endif // LEVEL3_H
