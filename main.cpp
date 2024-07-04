812854dad1b30690e7157c2ab546693e7e5e64ca
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2024-07-04 10:49:02 +0800

    Add files via upload
    7.4 10:47 进度
    实现了开始界面，登录界面，设置界面，注册，开始到选择关卡界面的转跳

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
-#include<iostream>
-#include<vector>
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
