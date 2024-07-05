0150d22e488d3534b38b7251c9bd070a2d17b45c
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-05 08:34:27 +0800

    Update level1.h

--- level1.h
+++ level1.h
@@ -7,6 +7,9 @@ class level1 : public Level {
     Q_OBJECT
 
 public:
+    EnemyTank* enemy1=NULL;
+    EnemyTank* enemy2=NULL;
+    EnemyTank* enemy3=NULL;
     explicit level1(QWidget *parent = nullptr);
     void keyPressEvent(QKeyEvent* event);
     void paintEvent(QPaintEvent *event) override;
