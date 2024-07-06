872ba9f939fefe50e0a99fb6d066f72bc4f872a3
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-06 10:08:29 +0800

    Update passWindow.h

--- passWindow.h
+++ passWindow.h
@@ -7,18 +7,25 @@
 #include"level3.h"
 #include"level4.h"
 
+namespace Ui {
+class pass;
+}
 
-class PassWindow : public QWidget
+class pass : public QWidget
 {
     Q_OBJECT
+
 public:
     QPushButton * backbtn;
 
-    explicit PassWindow(QWidget *parent = nullptr);
+    explicit pass(QWidget *parent = nullptr);
+    ~pass();
 
     //布尔数组
     bool pass_judge[4]={false,false,false,false};
 
+    //改变pass_judge的函数
+    void level1Win();
 
     void show_level();
     void setColor(QPushButton * btn);
@@ -28,8 +35,12 @@ public:
     void setupLevel1Button(QPushButton *button, level2 *level, QPushButton *nextButton);
     void setupLevel2Button(QPushButton *button, level3 *level, QPushButton *nextButton);
     void setupLevel3Button(QPushButton *button, level4 *level);
+    void setButtonStyle(QPushButton *btn);
 signals:
+    void back();
 
+private:
+    Ui::pass *ui;
 };
 
 #endif // PASS_H
