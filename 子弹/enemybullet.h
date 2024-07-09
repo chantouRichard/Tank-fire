b8bf1b1c1ab9a5ee1d45a58ba0e75474c6cb5415
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-10 00:10:12 +0800

    Update enemybullet.h

--- 子弹/enemybullet.h
+++ 子弹/enemybullet.h
@@ -8,10 +8,17 @@ class enemybullet : public Bullet
 {
     Q_OBJECT
 public:
+//    EnemyTank* parent;
+    int enemyx; //记录对面阵营的x坐标
+    int enemyy; //记录对面阵营的y坐标
     explicit enemybullet(QWidget *parent = nullptr);
-    void bulletboom(int boomsitx, int boomsity) override;
+    void bulletboom(int boomsitx, int boomsity);
+    bool checkshootenemy() override;
+    void getenemysit(int x, int y);
+    bool checkmovebullet() override;
+//    void set_parent(EnemyTank* parent);
 signals:
-
+    void kill_my_tank();
 };
 
 #endif // ENEMYBULLET_H
