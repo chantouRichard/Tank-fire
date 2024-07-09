ebcac0ad9646a3f35f629878e93d861a9170781b
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-10 00:13:34 +0800

    Update tank.h

--- 坦克/tank.h
+++ 坦克/tank.h
@@ -10,6 +10,7 @@
 #include <QLabel>
 #include <QVector>
 #include "bullet.h"
+#include <QTimer>
 QT_BEGIN_NAMESPACE
 
 QT_END_NAMESPACE
@@ -28,22 +29,41 @@ public:
     int maptankx2;int maptanky2; //标记坦克的地图位置
     int maptankx3;int maptanky3; //标记坦克的地图位置
     int maptankx4;int maptanky4; //标记坦克的地图位置
+    int startedmup;int startedmdo;
+    int startedmle;int startedmri;
+    void updatestarted();
+    QTimer *movetimer;
     QLabel *tank_img;
     Bullet bugdet[5];
     QPixmap TANK;
-    int MAP[Mapx_size][Mapy_size];
     Tank(int init_tank_x,int init_tank_y,QWidget *parent = nullptr);
-    void Loadmap(int map[25][25]);
     void showtank(QWidget *pa);//生成坦克
     void keyPressEvent(QKeyEvent *event) override;
-    void MOVE(QKeyEvent *event,int x,int y);//坦克的移动
+    void keyReleaseEvent(QKeyEvent *event) override;
+    void MOVE(QKeyEvent *event);//坦克的移动
     bool irremovable();   //检查坦克是否可以移动
     void shoot(QKeyEvent *event);       //坦克的射击
-    void addbudget();//填充子弹（将子弹设为当前地图）
     void updatemapsit();
-signals:
-    void move(int x,int y);
-private:
+    void Deletebullets();
+    int my_tank_live;
+    int my_tank_speed;//坦克速度，越小越快
 
+    //
+    void move_up();
+    void move_down();
+    void move_left();
+    void move_right();
+    void check_get_prop();
+    //道具属性
+    void addLife(int lifenumber);
+    void shield();
+    void attack();
+    void speedUp();
+signals:
+    void my_tank_shoot();
+    void my_tank_move(int x,int y);
+    void get_prop(int id);
+    void use_prop(int index);
+public slots:
 };
 #endif // WIDGET_H
