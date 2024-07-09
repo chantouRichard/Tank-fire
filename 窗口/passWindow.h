c40428e0da2e597aa6c33b323adb9fb1eeddb2f4
chantouRichard <148974199+chantouRichard@users.noreply.github.com>
2024-07-09 23:15:59 +0800

    Update passWindow.h

--- 窗口/passWindow.h
+++ 窗口/passWindow.h
@@ -6,29 +6,68 @@
 #include"level2.h"
 #include"level3.h"
 #include"level4.h"
+#include<QMovie>
 
+namespace Ui {
+class pass;
+}
 
-class PassWindow : public QWidget
+class pass : public QWidget
 {
     Q_OBJECT
+
 public:
     QPushButton * backbtn;
 
-    explicit PassWindow(QWidget *parent = nullptr);
+    explicit pass(QWidget *parent = nullptr);
+    ~pass();
 
     //布尔数组
     bool pass_judge[4]={false,false,false,false};
 
-
+    //改变pass_judge的函数
+    void level1Win();
     void show_level();
     void setColor(QPushButton * btn);
     void colorChange(QPushButton * btn);
-
-    void setupLevel0Button(QPushButton *button, level1 *level, QPushButton *nextButton);
-    void setupLevel1Button(QPushButton *button, level2 *level, QPushButton *nextButton);
-    void setupLevel2Button(QPushButton *button, level3 *level, QPushButton *nextButton);
+    int num=0;
+    void setupLevel0Button(QPushButton *button, level1 *level, QPushButton *nextButton,QPushButton *nextNew);
+    void setupLevel1Button(QPushButton *button, level2 *level, QPushButton *nextButton,QPushButton *nextNew);
+    void setupLevel2Button(QPushButton *button, level3 *level, QPushButton *nextButton,QPushButton *nextNew);
     void setupLevel3Button(QPushButton *button, level4 *level);
+    void setButtonStyle(QPushButton *btn);
+    void HideAllGif();
+    void HideAllButton();
+    void StartButtonStyle(QPushButton *button);
+    QLabel *gifLabel1;
+    QLabel *gifLabel2;
+    QLabel *gifLabel3;
+    QLabel *gifLabel4;
+    QMovie *movie1;
+    QMovie *movie2;
+    QMovie *movie3;
+    QMovie *movie4;
+    QPushButton *l1New;
+    QPushButton *l2New;
+    QPushButton *l3New;
+    QPushButton *l4New;
+    QPushButton *l1btn;
+    QPushButton *l2btn;
+    QPushButton *l3btn;
+    QPushButton *l4btn;
 signals:
+    void back();
+
+
+
+private:
+    bool isConnected1;
+    bool isConnected2;
+    bool isConnected3;
+    bool isConnected4;
+private slots:
+    void playGif(QMovie *movie,QLabel *gifLabel);
+    void checkFrame(QMovie *movie);
 
 };
 
