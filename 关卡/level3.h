325a0a65efd3a0b0f4125f2dc5ed8da0a8f7788c
Jay7982024 <161991218+Jay7982024@users.noreply.github.com>
2024-07-12 10:50:05 +0800

    Update level3.h

--- 关卡/level3.h
+++ 关卡/level3.h
@@ -9,11 +9,28 @@ class level3 : public Level
 public:
     explicit level3(QWidget *parent = nullptr);
     void paintEvent(QPaintEvent *event) override;
+    void keyPressEvent(QKeyEvent* event);
+    void Initmap();
+    QLabel *timeLabel;
+    QLabel *scoreLabel;
+    QTimer *timer;
+    int gameTime;
+    int gameScore;
+    QPixmap p0;
+    QPixmap p1;
+    QPixmap p2;
+    QPixmap p3;
+    QPixmap p4;
+    QPixmap p5;
+    QPixmap p6;
+
+    QLabel * b1;
+    QLabel * b2;
+    QLabel * b3;
+    QLabel * b4;
+    QLabel * b5;
 
 private:
-
-    void initTank() override;
-    void initEnemyTank() override;
     void dead() override;
 
 };
