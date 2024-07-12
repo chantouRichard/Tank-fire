b0675429c3d76d348b7466c1dec8cc1055df91d3
Jay7982024 <161991218+Jay7982024@users.noreply.github.com>
2024-07-12 10:48:24 +0800

    Update level1.h

--- 关卡/level1.h
+++ 关卡/level1.h
@@ -1,24 +1,36 @@
 #ifndef LEVEL1_H
 #define LEVEL1_H
-
+#include"global.h"
 #include "level.h"
 #include<QKeyEvent>
 class level1 : public Level {
     Q_OBJECT
 
 public:
-    EnemyTank* enemy1=NULL;
-    EnemyTank* enemy2=NULL;
-    EnemyTank* enemy3=NULL;
     explicit level1(QWidget *parent = nullptr);
-    void keyPressEvent(QKeyEvent* event) override;
+    void keyPressEvent(QKeyEvent* event)override;
     void paintEvent(QPaintEvent *event) override;
-    void updatemapforboom(int mapx, int mapy) override;
     void Initmap();
-    void Deletetank();
+
+    //
+    QLabel *timeLabel;
+    QLabel *Label1;
+    QLabel *Label2;
+    QLabel *Label3;
+    QLabel *Label4;
+    QLabel *Label5;
+
+    QPixmap pixWall0;
+    QPixmap pixWall1;
+    QPixmap pixWall2;
+    QPixmap p1;
+    QPixmap p2;
+
+    QLabel *scoreLabel;
+    QTimer *timer;
+    int gameTime;
+    int gameScore;
 private:
-    void initTank() override;
-    void initEnemyTank() override;
     void dead() override;
 };
 
